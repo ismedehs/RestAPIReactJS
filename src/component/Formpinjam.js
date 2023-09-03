1029621a34e3728aab32cef05b9ed85f1a7d1e3b
ismedeko1425 <ismedekohadisaputra@gmail.com>
2023-09-03 18:09:36 +0700

    update

--- src/component/Formpinjam.js
+++ src/component/Formpinjam.js
@@ -31,15 +31,24 @@ function Formpinjam({daftarbuku}) {
                 'Access-Control-Allow-Origin': '*',
                 'APIkey' : 'eyJ4NXQiOiJOVGRtWmpNNFpEazNOalkwWXpjNU1tWm1PRGd3TVRFM01XWXdOREU1TVdSbFpEZzROemM0WkE9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbkBzYW1wYW5na2FiLmdvLmlkIiwiYXBwbGljYXRpb24iOnsib3duZXIiOiJhZG1pbkBzYW1wYW5na2FiLmdvLmlkIiwidGllclF1b3RhVHlwZSI6bnVsbCwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJEZWZhdWx0QXBwbGljYXRpb24iLCJpZCI6MTI5OSwidXVpZCI6IjJkMmE5NDU5LWYzMWUtNDNkMy1iODkxLTc3OWI2NzUzMTlkZCJ9LCJpc3MiOiJodHRwczpcL1wvc3BscC5sYXlhbmFuLmdvLmlkOjQ0M1wvb2F1dGgyXC90b2tlbiIsInRpZXJJbmZvIjp7IlVubGltaXRlZCI6eyJ0aWVyUXVvdGFUeXBlIjoicmVxdWVzdENvdW50IiwiZ3JhcGhRTE1heENvbXBsZXhpdHkiOjAsImdyYXBoUUxNYXhEZXB0aCI6MCwic3RvcE9uUXVvdGFSZWFjaCI6dHJ1ZSwic3Bpa2VBcnJlc3RMaW1pdCI6MCwic3Bpa2VBcnJlc3RVbml0IjpudWxsfX0sImtleXR5cGUiOiJQUk9EVUNUSU9OIiwicGVybWl0dGVkUmVmZXJlciI6IiIsInN1YnNjcmliZWRBUElzIjpbeyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoic2FtcGFuZ2thYi5nby5pZCIsIm5hbWUiOiJTaWJhcmlzX1NhbXBhbmciLCJjb250ZXh0IjoiXC90XC9zYW1wYW5na2FiLmdvLmlkXC9hc2V0X2RhZXJhaF9zYW1wYW5nXC8xLjAiLCJwdWJsaXNoZXIiOiJhZG1pbkBzYW1wYW5na2FiLmdvLmlkIiwidmVyc2lvbiI6IjEuMCIsInN1YnNjcmlwdGlvblRpZXIiOiJVbmxpbWl0ZWQifSx7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJzYW1wYW5na2FiLmdvLmlkIiwibmFtZSI6IlBlamFiYXRfU2FtcGFuZyIsImNvbnRleHQiOiJcL3RcL3NhbXBhbmdrYWIuZ28uaWRcL2RhZnRhcl9wZWphYmF0X3NhbXBhbmdcLzEuMCIsInB1Ymxpc2hlciI6ImFkbWluQHNhbXBhbmdrYWIuZ28uaWQiLCJ2ZXJzaW9uIjoiMS4wIiwic3Vic2NyaXB0aW9uVGllciI6IlVubGltaXRlZCJ9LHsic3Vic2NyaWJlclRlbmFudERvbWFpbiI6InNhbXBhbmdrYWIuZ28uaWQiLCJuYW1lIjoiUGVycHVzX1RlcyIsImNvbnRleHQiOiJcL3RcL3NhbXBhbmdrYWIuZ28uaWRcL1BlcnB1c190ZXNcLzEuMCIsInB1Ymxpc2hlciI6ImFkbWluQHNhbXBhbmdrYWIuZ28uaWQiLCJ2ZXJzaW9uIjoiMS4wIiwic3Vic2NyaXB0aW9uVGllciI6IlVubGltaXRlZCJ9XSwidG9rZW5fdHlwZSI6ImFwaUtleSIsInBlcm1pdHRlZElQIjoiIiwiaWF0IjoxNjkzNzE2MTM1LCJqdGkiOiI5MDliMjNiNy0wZjZhLTRhODktODdhNC1jYjc2NmVkNDRjNzcifQ==.LoWn7p7hxvWuGVswP_Tq6MzBdKI9fEOgXHyVAmk8pNHQJAmLX9WmTHr8QlcTTdwY3Og9BGj4ogYNxaOFE3MCVN88d39yYDNol26E9M7TOflXW2BGWDEcna6KwssmfTqB1MzPjicp-KIRj_AooTSAHYUpUJKxp7FyXef-A4VOTT9w4MyH47l3HOoZO07GG9xuSHVrxReHHHemzN08Oe4klWDq7riaDBKzMZbEj4Uw9XlerZT17IGsf12OFkIKfjCL32j8BnjHCHe-1H2MOtiUW2IyF28afFWP76FB83xfjlyAF-VGaDzBHIt1eQBX76r2az2y4x9olA_q0pFv09dlIg=='
             }
-        }).then(response => {
-            if (response.status === 200) {
-                alert("New website saved successfully");
-            } else {
-             alert("gagal");
-            }
-        });
+        })
+        .then((res) => {
+         if (res.ok) {
+           // return res.json();
+           alert("Data Berhasil ditambahkan");
+         } else {
+          alert("Error fatching data");          
+         }
+       })
+        // .then(response => {
+        //     if (response.status === 200) {
+        //         alert("New website saved successfully");
+        //     } else {
+        //      alert("gagal");
+        //     }
+        // });
         //react convert object to string
-        alert(JSON.stringify({...inputs}));
+        // alert(JSON.stringify({...inputs}));
     }
 
   return (
